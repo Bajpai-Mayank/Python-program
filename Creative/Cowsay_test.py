2202098facfaeb9167d9d63fba8656c6c0d0af8e
Mayank23x <121786048+Mayank23x@users.noreply.github.com>
2026-05-27 08:46:21 +0530

    update

--- Creative/Cowsay_test.py
+++ Creative/Cowsay_test.py
@@ -3,5 +3,5 @@ import cowsay
 cowsay.cow("Gololo Morningo !")
 cowsay.dragon("I'm mIgHtY FlamY")
 cowsay.tux("Sueeeee!!!!!!!")
-cowsay.beavis("Who taught them English! ah no hell!!")
-cowsay.daemon("Yeahi ! I taughy thea me , I'm gonius BiROOOOOOO!")
+cowsay.beavis("I'm beavis")
+cowsay.daemon("Yeah !Hello Everyone!")
